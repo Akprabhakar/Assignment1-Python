4e77174aa0efaa52fa99269115920969f8361c62
Akprabhakar <akbiomatrix@gmail.com>
2025-06-07 13:09:15 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,5 @@
+# Creating a Personalized Greeting
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+
+print("Hello, " + name1 + " " + name2 + "! Welcome to the Python program.")
